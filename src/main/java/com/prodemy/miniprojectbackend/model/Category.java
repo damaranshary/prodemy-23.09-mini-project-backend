c888a6f9c9a44cee3a4b98a62d411ababaf1e938
Damar Anshary <damar.galihanshary7@gmail.com>
2024-01-02 20:18:25 +0700

    defining DDL schema

--- src/main/java/com/prodemy/miniprojectbackend/model/Category.java
+++ src/main/java/com/prodemy/miniprojectbackend/model/Category.java
@@ -1,4 +1,23 @@
 package com.prodemy.miniprojectbackend.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "t_category")
 public class Category {
+    @Id
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
 }
--- src/main/java/com/prodemy/miniprojectbackend/model/Product.java
+++ src/main/java/com/prodemy/miniprojectbackend/model/Product.java
@@ -1,4 +1,32 @@
 package com.prodemy.miniprojectbackend.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "t_product")
 public class Product {
+    @Id
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "image")
+    private String image;
+
+    @Column(name = "price")
+    private Long price;
+
+    @Column(name = "category_id")
+    private Long categoryId;
 }
